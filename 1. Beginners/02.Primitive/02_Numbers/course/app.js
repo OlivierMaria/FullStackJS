e410af529e662641c62467102ff95dac72776c85
OlivierMaria <olivier-maria@hotmail.com>
2023-11-15 12:02:55 +0100

    02.Primitive > 02_Numbers > Course and exercise done

---
+++ 1. Beginners/02.Primitive/02_Numbers/course/app.js
@@ -0,0 +1,26 @@
+// JavaScript has one number type
+// Positive Number
+// Negative Number
+// Whole Number (integers)
+// Decimal Numbers
+
+const randomNumber = 10200;
+const favNumber = 21;
+let num = 2;
+
+// Basic Math
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+console.log(num++);
+console.log(num--);
+
+let counter = 0;
+counter++;
+counter++;
+counter++;
+counter--;
+console.log(counter);
