eae66a63840016bf3d60c617a2b955adc0aaf22b
OlivierMaria <olivier-maria@hotmail.com>
2023-11-15 11:11:17 +0100

    02.Primitive > 01_Variables > Course and exercise done

---
+++ 02.Primitive/01_Variables/course/app.js
@@ -0,0 +1,17 @@
+// Varianles
+
+// Quantité de valeur qui peut être modifiée pendant l'exécution du programme. C'est une boites ou nous stockons les données.
+// (Reserved keyword) (Variable name) (Type of value)
+
+// Declared variable
+let name;
+
+// Assign value
+
+let firstName;
+firstName = "Olivier";
+console.log(firstName);
+
+// Var : Ancienne convention
+// Const : Valeurs constante ne pouvant être modifié
+// Let : Valeurs pouvant être modifié
