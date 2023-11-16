74dcee4c10e9ad8b29de57926bf7a8d0919317bd
OlivierMaria <olivier-maria@hotmail.com>
2023-11-16 05:25:51 +0100

    02.Primitive > 03_Booleans > Course and exercise done

---
+++ 1. Beginners/02.Primitive/03_Booleans/course/app.js
@@ -0,0 +1,13 @@
+let isLoggedIn = NaN;
+let numbers = 10;
+console.log(numbers + undefined);
+
+// -> Falsy value
+
+// false
+// null
+// undefined
+// 0
+// -0
+// NaN
+// '', "", ``, (empty strings)
