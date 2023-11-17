1feec0e5844ba88eaa81ba57678232e4cdaafae3
OlivierMaria <olivier-maria@hotmail.com>
2023-11-17 09:34:30 +0100

    03.ControlFlow > 01_ConditionalStatements > Course and exercise done

--- 1. Beginners/02.Primitive/01_Variables/course/app.js
+++ 1. Beginners/02.Primitive/01_Variables/course/app.js
@@ -1,17 +1,7 @@
-// Varianles
+// (reserved keyword): (variable name): (type of value)
 
-// Quantité de valeur qui peut être modifiée pendant l'exécution du programme. C'est une boites ou nous stockons les données.
-// (Reserved keyword) (Variable name) (Type of value)
-
-// Declared variable
-let name;
-
-// Assign value
-
-let firstName;
-firstName = "Olivier";
-console.log(firstName);
-
-// Var : Ancienne convention
-// Const : Valeurs constante ne pouvant être modifié
-// Let : Valeurs pouvant être modifié
+let variable; // undefined
+let name = "Olivier";
+console.log(name);
+const channelName = "Olidemy";
+console.log(channelName);
