82edd5537b1009ad4560d256c6cd122a1b4b3c6b
OlivierMaria <olivier-maria@hotmail.com>
2023-11-19 14:07:29 +0100

    04.ReferenceTypes > 02_ArrayBasicMethods > Course and exercise done + add README

---
+++ 1. Beginners/04.ReferenceTypes/02_ArrayBasicMethods/exercise/app.js
@@ -0,0 +1,38 @@
+// concat()	joins two or more arrays and returns a result
+// includes() checks if an array contains a specified element
+// push() adds a new element to the end of an array and returns the new length of an array
+// unshift() adds a new element to the beginning of an array and returns the new length of an array
+// pop() removes the last element of an array and returns the removed element
+// shift() removes the first element of an array and returns the removed element
+// sort() sorts the elements alphabetically in strings and in ascending order
+// slice() selects the part of an array and returns the new array
+// splice() removes or replaces existing elements and/or adds new elements
+
+const myFavFootballPlayers = ["Ronaldo", "Messi", "Zidane"];
+const myFavNumbers = [47, 12, 24];
+
+total = myFavFootballPlayers.concat(myFavNumbers);
+// console.log(total);
+
+// console.log(myFavFootballPlayers.includes("Ronaldo"));
+
+// console.log(myFavFootballPlayers.push("Oli"));
+// console.log(myFavFootballPlayers);
+
+// console.log(myFavFootballPlayers.unshift("Oli"));
+// console.log(myFavFootballPlayers);
+
+// console.log(myFavFootballPlayers.pop());
+// console.log(myFavFootballPlayers);
+
+// console.log(myFavFootballPlayers.shift());
+// console.log(myFavFootballPlayers);
+
+// console.log(myFavNumbers);
+// console.log(myFavNumbers.sort());
+
+// console.log(myFavNumbers);
+// console.log(myFavNumbers.slice([2]));
+
+// console.log(myFavFootballPlayers.splice([1]));
+// console.log(myFavFootballPlayers);
