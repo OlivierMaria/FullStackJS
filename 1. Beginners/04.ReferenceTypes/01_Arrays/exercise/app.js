b0939f49e8e2795ed248d5a89fdcc55388f2674e
OlivierMaria <olivier-maria@hotmail.com>
2023-11-19 13:05:52 +0100

    04.ReferenceTypes > 01_Arrays > Course and exercise done

---
+++ 1. Beginners/04.ReferenceTypes/01_Arrays/exercise/app.js
@@ -0,0 +1,13 @@
+// 1. Create array name (favSingers) Store 3 favorite singers.
+// 2. log the first singer in that array.
+// 3. Create array name (favNumbers) & store 4 fav Numbers.
+// 4. Create array name (mixedArr) store ["string", ["otherarray"], 123, true]
+// 5. Now Access each item in that array by using [] notation.
+
+const favSingers = ["Michael Jackson", "Elvis Presley", "John Lennon"];
+console.log(favSingers[0]);
+
+const favNumbers = [8, 12, 24, 32];
+const mixedArr = ["string", ["OtherArray", "each"], 123, true];
+console.log(favNumbers);
+console.log(mixedArr[1][1]);
