112fa8f533a9e78e745c8512cc4b4e707d42ab5c
OlivierMaria <olivier-maria@hotmail.com>
2023-11-24 11:15:11 +0100

    2.Intermediate > 01.TemplateString > Course and exercise done

---
+++ 2.Intermediate/01.TemplateString/exercise/app.js
@@ -0,0 +1,20 @@
+// 1. Store this in template string ~
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+
+// 2. Create two variables (firstName, lastName)
+// 3. Use that variables in template string.
+
+const text = `The quick
+brown fox
+jumps over
+the lazy dog`;
+
+console.log(text);
+
+const firstName = "Olivier";
+const lastName = "Maria";
+
+console.log(`My name is: ${firstName} ${lastName}`);
